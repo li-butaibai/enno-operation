7ab1e3f334dbbd4d7454ba889b635e1e21120db0
EriclLee <lizongwen@live.cn>
2015-11-17 16:36:28 +0800

    add controller

---
+++ enno-operation-web/src/main/java/enno/operation/web/controllers/EventLogController.java
@@ -0,0 +1,53 @@
+package enno.operation.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by v-zoli on 2015/11/17.
+ */
+@Controller
+@RequestMapping(value = "/eventlogs")
+public class EventLogController {
+    @RequestMapping(value = {"","/","/list"}, method = RequestMethod.GET)
+    public ModelAndView list(int pageIndex, int pageSize)
+    {
+        ModelAndView modelAndView = new ModelAndView("/eventsources/list");
+        try {
+            //TODO£º
+            modelAndView.addObject("success", true);
+        }
+        catch (Exception ex)
+        {
+            modelAndView.addObject("success",false);
+        }
+        finally {
+            return modelAndView;
+        }
+    }
+
+    @RequestMapping(value="/detail/{eventLogId}", method = RequestMethod.GET)
+    public Map<String, Object> detail(String eventSourceId)
+    {
+        Map<String, Object> model = new HashMap<String,Object>();
+        try{
+            //TODO£º
+            model.put("success",true);
+            return model;
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            model.put("success",false);
+            return model;
+        }
+        finally {
+            return model;
+        }
+    }
+}
