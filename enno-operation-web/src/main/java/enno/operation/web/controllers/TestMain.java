992dd8d8e6f973a6d30ef32b759631a3b837991c
EriclLee <lizongwen@live.cn>
2015-11-17 15:51:29 +0800

    el

---
+++ enno-operation-web/src/main/java/enno/operation/web/controllers/TestMain.java
@@ -0,0 +1,22 @@
+package enno.operation.web.controllers;
+
+import com.sun.corba.se.impl.presentation.rmi.ExceptionHandlerImpl;
+import enno.operation.core.model.EventSourceModel;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * Created by v-zoli on 2015/11/17.
+ */
+public class TestMain {
+    public static void main(String[] args)
+    {
+        try {
+            EventSourceController eventSourceController = new EventSourceController();
+            ModelAndView d = eventSourceController.list(0, 10);
+            System.in.read();
+        }catch (Exception ex)
+        {ex.printStackTrace();}
+    }
+}
--- enno-operation-web/target/Web/WEB-INF/jsp/eventsources/list.jsp
+++ enno-operation-web/target/Web/WEB-INF/jsp/eventsources/list.jsp
@@ -1,6 +1,23 @@
 
 <%@ include file="../include/top.inc"%>
-<div>
-  <h1>list</h1>
+<div class="wrapper page">
+  <div class="topbar">
+    <div class="breadcrumbs">
+      <a class="level level-zone gd1 pjaxA" href="~/cloud/index" data-permalink="">Event Source</a>
+    </div>
+  </div>
+  <div class="pane">
+    <div class="toolbar">
+    <a class="btn" href="#" onclick="createTenant()">
+      <span class="icon icon-run" ></span>
+      <span class="text">Add</span>
+    </a>
+    <a class="btn" href="#">
+      <span class="text">Delete</span>
+    </a>
+  </div>
+    ${}
+  </div>
 </div>
+
 <%@ include file="../include/bottom.inc"%>
